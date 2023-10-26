aff122aba13b31edc214cb6ad37a3574c8e34c96
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-10-26 18:18:43 +0300

    update

--- holo.sh
+++ holo.sh
@@ -8,10 +8,13 @@ fi
 function installNodeJS {
 	echo -e '\n\e[42mPreparing to install\e[0m\n' && sleep 1
 	cd $HOME
-	sudo apt update && apt upgrade -y
-    sudo apt install -y curl git jq lz4 build-essential unzip < "/dev/null"
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-	apt install nodejs -y
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+	NODE_MAJOR=20
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
+    sudo apt-get install nodejs -y
   sleep 1
 }
 
