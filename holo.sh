7b076e335cf9d004ce225f7303afe5649e2ce169
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-10-26 17:09:05 +0300

    update

--- holo.sh
+++ holo.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if exists curl; then
-	echo ''
-else
-  sudo apt install curl -y < "/dev/null"
-fi
+sudo apt install curl -y < "/dev/null"
 bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
