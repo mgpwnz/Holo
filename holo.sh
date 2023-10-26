80e2ea7c092baa6385fa708ad03048518e0b8a97
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-10-26 19:07:47 +0300

    exit

--- holo.sh
+++ holo.sh
@@ -5,7 +5,7 @@ do
 # Menu
 
 PS3='Select an action: '
-options=("NodeJS" "Install" "Run Operator" "Faucet" "Logs" "Quit")
+options=("NodeJS" "Install" "Run Operator" "Faucet" "Logs" "Exit")
 select opt in "${options[@]}"
                do
                    case $opt in                          
